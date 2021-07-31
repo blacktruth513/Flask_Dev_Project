6bbec25eab258f41ff1705802a00f5cd0bced953
HyunJin Choi <78430460+blacktruth513@users.noreply.github.com>
2021-07-31 23:58:43 +0900

    Delete flask_project directory

--- flask_project/config.py
+++
@@ -1,9 +0,0 @@
-db = {
-    'user': 'root',
-    'password': 'dlsrb@41632',
-    'host': '49.50.166.134',
-    'port': '3306',
-    'database': 'Flask_Project'}
-
-DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}\
-        :{db['port']}/{db['database']}?charset=utf8"
